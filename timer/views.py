b94a5d5801986bb1a174ff2d522bd3fd753ddcd8
اسم شما <lotfimaryam1110@gmail.com>
2025-02-24 14:51:07 -0900

    your commit views

--- timer/views.py
+++ timer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import TimeEntry, Profile
+from .serializers import TimeEntrySerializer, ProfileSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
 
-# Create your views here.
+class TimeEntryListCreateView(generics.ListCreateAPIView):
+    queryset = TimeEntry.objects.all()
+    serializer_class = TimeEntrySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class TimerViewSet(viewsets.ModelViewSet):
+
+    queryset = TimeEntry.objects.all()
+    serializer_class = TimerSerializer
+    permission_classes = [IsAuthenticated]
