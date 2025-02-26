cadc21f6da5a0e38f14f2df3c900b942dcbcd4f1
ali <alikeshavarztoa@gmail.com>
2025-02-26 15:50:36 +0330

    auth added to app authentication

--- timer/urls.py
+++ timer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import TimeEntryListCreateView, TimeEntryRetrieveUpdateDestroyView
 
+
 urlpatterns = [
     path('time-entries/', TimeEntryListCreateView.as_view(), name='time-entry-list-create'), # This URL is used for both creating a new time entry (POST) and listing existing time entries (GET)
     path('time-entries/<int:pk>/', TimeEntryRetrieveUpdateDestroyView.as_view(), name='time-entry-detail'), # To retrieve, update, or delete a specific entry.
