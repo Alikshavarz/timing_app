b1fbb14d0821e17473a6302d14c811fc13659cfb
ali <alikeshavarztoa@gmail.com>
2025-02-27 15:58:22 +0330

    coplated app authentication user

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,16 @@
 import uuid
+import string
+import random
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserSerializer
 from .models import CustomUser
-from rest_framework.permissions import IsAuthenticated
 
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
 
 class AuthView(APIView):
     def post(self, request):
@@ -21,7 +25,7 @@ class AuthView(APIView):
             user = CustomUser.objects.get(username=device_id)
             is_new_user = False
         except CustomUser.DoesNotExist:
-            password = CustomUser.objects.make_random_password()
+            password = generate_random_password()
             user = CustomUser.objects.create_user(
                 username=device_id,
                 password=password
@@ -29,19 +33,21 @@ class AuthView(APIView):
             is_new_user = True
 
         refresh = RefreshToken.for_user(user)
-
+        
         return Response({
             'refresh': str(refresh),
             'access': str(refresh.access_token),
-            'user_id': user.id_user,
+            'user_id': str(user.id_user), 
             'device_id': device_id,
             'is_new_user': is_new_user
         }, status=status.HTTP_201_CREATED if is_new_user else status.HTTP_200_OK)
 
-
 class UserDetailView(APIView):
-    permission_classes = [IsAuthenticated]  # اطمینان از اعتبار توکن
-
     def get(self, request):
-        serializer = UserSerializer(request.user)
-        return Response(serializer.data)
+        try:
+            serializer = UserSerializer(request.user)
+            return Response(serializer.data)
+        except Exception:
+            return Response({
+                'error': 'Invalid token'
+            }, status=status.HTTP_401_UNAUTHORIZED)
