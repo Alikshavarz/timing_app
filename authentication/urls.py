1ce7fc7fd98e1cc8746994cbdf7b308cbd0f228e
ali <alikeshavarztoa@gmail.com>
2025-03-04 18:49:16 +0330

    runs api auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import AuthView, UserDetailView
+from .views import AuthView, UserDetailView , RefreshToken
 urlpatterns = [
     path('auth/', AuthView.as_view(), name='auth'),  # URL برای AuthView
     path('user/', UserDetailView.as_view(), name='user-detail'),  # URL برای UserDetailView
+    # path('token/refresh/', RefreshToken.as_view(), name='token_refresh')
 ]
--- timer/urls.py
+++ timer/urls.py
@@ -1,12 +1,10 @@
 from .views import TimeEntryListCreateView, TimeEntryRetrieveUpdateDestroyView
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from timer.views import TimeEntryListCreateView, TimeEntryRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('time-entries/', TimeEntryListCreateView.as_view(), name='time-entry'),
     path('time-entries/<int:pk>/', TimeEntryRetrieveUpdateDestroyView.as_view(), name='time-entry-detail'), # To retrieve, update, or delete a specific entry.
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # برای دریافت توکن
-
 ]
